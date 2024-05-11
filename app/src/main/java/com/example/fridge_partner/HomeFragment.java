59056cc992fdc4dad1f61cb7afa7ca52f47b9a4c
WendyRl <wendyrl20000131@gmail.com>
2024-05-11 04:41:16 -0700

    add the feature of adding fridge details including name, description, rows and also reset & create button

--- app/src/main/java/com/example/fridge_partner/HomeFragment.java
+++ app/src/main/java/com/example/fridge_partner/HomeFragment.java
@@ -2,7 +2,7 @@ package com.example.fridge_partner;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +14,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.example.fridge_partner.CreateFridgePopWin;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
